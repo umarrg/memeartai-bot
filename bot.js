d2e6f07dfb0ad032612f6355667b4582b6313aeb
umarrg <umarabubakarfta@gmail.com>
2025-05-22 14:09:56 +0100

    added some fixes

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ bot.setMyCommands(commands);
 bot.onText(/\/start/, (msg) => {
     let name = msg.chat.first_name;
     const chatId = msg.chat.id;
-    const welcomeMessage = `Hey ${name}! \n\nWelcome to Memeartai AI Image Generator Bot! Here are some commands you can use: \n\n - /start: Start the bot \n - /help: Display this help message  \n - /create: Generate an image from prompt /create monad seals < prompt > \n`;
+    const welcomeMessage = `Hey ${name}! \n\nWelcome to Memeartai AI Image Generator Bot! Here are some commands you can use: \n\n - /start: Start the bot \n - /help: Display this help message  \n - /create: Generate an image from prompt /create kad < prompt > \n`;
 
 
     bot.sendMessage(chatId, welcomeMessage,);
@@ -38,7 +38,7 @@ bot.onText(/\/start/, (msg) => {
 bot.onText(/\/help/, (msg) => {
     let name = msg.chat.first_name;
     const chatId = msg.chat.id;
-    const helpMessage = `Hey ${name}! \n\nWelcome to Memeartai AI Image Generator Bot! Here are some commands you can use: \n\n - /start: Start the bot \n - /help: Display this help message  \n - /create: Generate an image from prompt /create monad seals < prompt > \n`;
+    const helpMessage = `Hey ${name}! \n\nWelcome to Memeartai AI Image Generator Bot! Here are some commands you can use: \n\n - /start: Start the bot \n - /help: Display this help message  \n - /create: Generate an image from prompt /create kad < prompt > \n`;
 
 
 
@@ -57,10 +57,11 @@ bot.onText(/\/create (.+)/, async (msg, match) => {
                 prompt: prompt,
                 model_name: null,
                 loras: [{
-                    path: "https://v3.fal.media/files/rabbit/DXUTCNRAiEbEEsHnAF0_H_pytorch_lora_weights.safetensors",
+                    path: "https://v3.fal.media/files/lion/rNzzbUh2xYtO3CXLNmdJC_pytorch_lora_weights.safetensors",
                     scale: 1
                 }],
                 embeddings: [],
+
                 enable_safety_checker: false
 
 
